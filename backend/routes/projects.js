8c476d8ba0c95fcc946cb73e07a470101256c8a1
Hakan <hakanonize35@gmail.com>
2021-08-31 15:19:01 +0300

    Trial

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -1,30 +1,30 @@
 const router = require('express').Router();
 let Project = require('../models/project.model');
 
-router.route('/').get((req,res) => {
-    Project.find()
-            .then(projects => res.json(projects))
-            .catch(err => res.status(404).json('Error' + err));
+router.route('/').get((req, res) => {
+  Project.find()
+    .then((projects) => res.json(projects))
+    .catch((err) => res.status(404).json('Error' + err));
 });
 
-router.route('/add').post((req,res) => {
-    const pname = req.body.pname;
-    const shortex = req.body.shortex;
-    const pdescription = req.body.pdescription;
-    //const pcreator = req.currentUser;
-    const pqualifications = req.body.pqualifications;
+router.route('/add').post((req, res) => {
+  const pname = req.body.pname;
+  const shortex = req.body.shortex;
+  const pdescription = req.body.pdescription;
+  //const pcreator = req.currentUser;
+  const pqualifications = req.body.pqualifications;
 
-    const newProject = new Project({
-        pname,
-        shortex,
-        pdescription,
-        pqualifications
-    });
+  const newProject = new Project({
+    pname,
+    shortex,
+    pdescription,
+    pqualifications,
+  });
 
-    newProject.save()
-              .then(() => res.json ('Project added'))
-              .catch(err => res.status(404).json('Error'+ err));
-
+  newProject
+    .save()
+    .then(() => res.json('Project added'))
+    .catch((err) => res.status(404).json('Error' + err));
 });
 
-    module.exports = router;
+module.exports = router;
